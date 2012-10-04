c46f1307ccf947ed4303e911c282a96253a35c5d
Ryan Kelly <ryan@rfk.id.au>
2012-10-05 09:52:52 +1000

    Fleshing out pyramid+persona+macauth blog post.

--- content/blog/entry/securing-pyramid-persona-macauth/luckynum_client.py
+++ content/blog/entry/securing-pyramid-persona-macauth/luckynum_client.py
@@ -1,20 +1,20 @@
-
 import requests
 import macauthlib
 
-
+# A set or previously-provisioned MACAuth credentials.
 CREDENTIALS = {
-  "id": "eyJzYWx0IjogIjE3MThmMSIsICJleHBpcmVzIjogMTM0OTM1MDIwMC40MDM1NzMsI"
-        "CJ1c2VyaWQiOiAicnlhbkByZmsuaWQuYXUifRCtAtGwIml5Fk6vaL2uvRQchPMC",
-  "key": "p9tYCJ6TGaWbZq9GJq_DalpZmIg="
+  "id": "eyJzYWx0IjogImNlMWM4YyIsICJleHBpcmVzIjogMTM0OTM5NDQ2My43OTIyOTUs"
+        "ICJ1c2VyaWQiOiAicnlhbkByZmsuaWQuYXUifWyyOtRhUCI9D4I9Oz0Tho7f4-FK",
+  "key": "DqvXadiE3QRySMRLnkGT5EmSvPw="
 }
 
-
+# Create a requests session with a hook to sign all outgoing requests.
+# The macauthlib package has native support for request's data types.
 def auth_hook(req):
     macauthlib.sign_request(req, **CREDENTIALS)
     return req
-
-
 session = requests.session(hooks={"pre_request": auth_hook})
+
+# Then we can easily script access to the service.
 print session.get("http://localhost:8080/").content
 
--- content/blog/entry/securing-pyramid-persona-macauth/luckynum_server_orig.py
+++ content/blog/entry/securing-pyramid-persona-macauth/luckynum_server_orig.py
@@ -15,15 +15,25 @@ Your lucky number for today is {number}.
 
  
 def lucky_number(request):
+    """Pyramid view to generate a lucky number."""
+
+    # Check that the user is authenticated.
     userid = authenticated_userid(request)
     if userid is None:
         raise Forbidden()
+
+    # Generate and return the lucky numbe.
     number = random.randint(1,100)
     return Response(TEMPLATE.format(**locals()), content_type="text/plain")
 
 
-if __name__ == "__main__":
+def main():
+    """Construct and return a WSGI app for the luckynumber service."""
+
     settings = {
+      # The pyramid_persona plugin needs as master secret to use for
+      # signing login cookies, and the expected hostname of your website
+      # to prevent fradulent login attempts.
       "persona.secret": "TED KOPPEL IS A ROBOT",
       "persona.audiences": "localhost:8080",
     }
@@ -32,8 +42,13 @@ if __name__ == "__main__":
     config.add_route("number", "/")
     config.add_view(lucky_number, route_name="number")
 
+    # Including pyramid_persona magically enables authentication.
     config.include("pyramid_persona")
+ 
+    return config.make_wsgi_app()
 
-    app = config.make_wsgi_app()
+
+if __name__ == "__main__":
+    app = main()
     server = wsgiref.simple_server.make_server("", 8080, app)
     server.serve_forever()
