b79be13af1aeff659755ffa2e64531e5f03d04b6
Ryan Kelly <ryan@rfk.id.au>
2012-10-05 10:40:08 +1000

    Releasing latest blog post

--- content/blog/entry/securing-pyramid-persona-macauth/luckynum_client.py
+++ content/blog/entry/securing-pyramid-persona-macauth/luckynum_client.py
@@ -1,7 +1,7 @@
 import requests
 import macauthlib
 
-# A set or previously-provisioned MACAuth credentials.
+# A set of previously-provisioned MACAuth credentials.
 CREDENTIALS = {
   "id": "eyJzYWx0IjogImNlMWM4YyIsICJleHBpcmVzIjogMTM0OTM5NDQ2My43OTIyOTUs"
         "ICJ1c2VyaWQiOiAicnlhbkByZmsuaWQuYXUifWyyOtRhUCI9D4I9Oz0Tho7f4-FK",
